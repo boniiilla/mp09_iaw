3ab1da11eb1a7de11350eab3e26f3b4cb37ae853
boniiilla <c.bonillac00@gmail.com>
2024-11-19 19:55:34 +0100

    php completed

--- contar_a/contar_a.php
+++ contar_a/contar_a.php
@@ -12,6 +12,24 @@ echo "<br/>";
 #lo mostramos:
 var_dump($frasearray);
 
+#a partir de aquí habrá que tratar el contenido y resolver...
+$cuentaA;
+
+$frase = 'Este es un ejemplo con varias letras A y a';
+$cuentaA = 0;
+
+// Convertimos la frase a un array de caracteres
+$frasearray = str_split($frase);
+
+// Recorremos el array para contar las 'a' y 'A'
+foreach ($frasearray as $letra) {
+    if (strtolower($letra) == 'a') { // No distingue entre mayúsculas y minúsculas
+        $cuentaA++;
+    }
+}
+
+echo "La letra 'a' (mayúscula o minúscula) aparece " . $cuentaA . " veces en la frase.";
+
 ?>
 </body>
 </html>
